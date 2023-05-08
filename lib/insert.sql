95dfbf4690f2e21bde1fe0f00deee2969adb5365
Halim Choi <hchoicareer@gmail.com>
2023-05-08 16:27:56 -0400

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ("Mr. Chocolate", 10, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Rowdy", 13, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Tabitha", 9, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Sergeant Brown", 15, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Melissa", 8, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Grinch", 16, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Wendy", 4, "F");
+INSERT INTO bears (name, age, sex) VALUES (NULL, NULL, NULL);
